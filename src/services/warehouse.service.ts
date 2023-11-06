4459db42cbf77c1f032c42ddb00f6c4aec424e8c
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-06 14:26:09 +0100

    Generate file access

--- src/services/warehouse.service.ts
+++ src/services/warehouse.service.ts
@@ -1,13 +1,13 @@
-import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable } from "@nestjs/common";
 import {
   TMWU_WAREHOUSE_SETTINGS_PROVIDER,
   WarehouseSettingsProvider,
-} from '../modules/warehouse.module';
-import { isFuture, addMinutes, isPast } from 'date-fns';
-import { randomString } from '../utils/string/random-string.util';
-import { AccessToken } from '../types/token/access-token.type';
-import { SignedToken } from '../types/token/signed-token.type';
-import NodeRSA from 'node-rsa';
+} from "../modules/warehouse.module";
+import { isFuture, addMinutes, isPast } from "date-fns";
+import { randomString } from "../utils/string/random-string.util";
+import { AccessToken } from "../types/token/access-token.type";
+import { SignedToken } from "../types/token/signed-token.type";
+import NodeRSA from "node-rsa";
 
 type SignOptions = {
   fileId: string;
@@ -18,27 +18,27 @@ type SignOptions = {
 export class WarehouseService {
   constructor(
     @Inject(TMWU_WAREHOUSE_SETTINGS_PROVIDER)
-    private readonly warehouseSettings: WarehouseSettingsProvider,
+    private readonly warehouseSettings: WarehouseSettingsProvider
   ) {}
 
-  public async generateSignedToken(
-    publicKey: string,
-    { fileId, expiresAt: definedExpiresAt }: SignOptions,
-  ) {
+  private async generateSignedToken({
+    fileId,
+    expiresAt: definedExpiresAt,
+  }: SignOptions) {
     if (definedExpiresAt && !isFuture(definedExpiresAt))
-      throw new Error('expiresAt must be a future time');
+      throw new Error("expiresAt must be a future time");
 
     const expiresAt = definedExpiresAt ?? addMinutes(new Date(Date.now()), 30);
 
     const signedToken = JSON.stringify({
       w: this.warehouseSettings.setupInfo.warehouseName,
       st: this.encryptWithPublicKey(
-        publicKey,
+        this.warehouseSettings.setupInfo.publicKey,
         JSON.stringify({
           expiresAt,
           fileId,
           salt: randomString(24),
-        } as SignedToken),
+        } as SignedToken)
       ),
     } as AccessToken);
 
@@ -46,7 +46,17 @@ export class WarehouseService {
     const encoder = new TextEncoder();
     const data = encoder.encode(signedToken);
 
-    return Buffer.from(data).toString('base64');
+    return Buffer.from(data).toString("base64");
+  }
+
+  public async generateFileAccess(options: SignOptions) {
+    const signedToken = await this.generateSignedToken(options);
+
+    return {
+      url: `${this.warehouseSettings.options.host}/warehouse/file?token=${signedToken}`,
+      token: signedToken,
+      warehouseHost: this.warehouseSettings.options.host,
+    };
   }
 
   public async decodeAccessToken(token: string, privateKey: string) {
@@ -56,8 +66,8 @@ export class WarehouseService {
     const { w, st }: AccessToken = JSON.parse(decodedJSONToken);
 
     const decodedToken = JSON.parse(
-      this.decryptWithPrivateKey(privateKey, st),
-    ) as Omit<SignedToken, 'expiresAt'> & { expiresAt: string };
+      this.decryptWithPrivateKey(privateKey, st)
+    ) as Omit<SignedToken, "expiresAt"> & { expiresAt: string };
 
     if (isPast(new Date(decodedToken.expiresAt)))
       throw new ForbiddenException();
@@ -69,12 +79,12 @@ export class WarehouseService {
 
   // RSA methods
   private encryptWithPublicKey(publicKey: string, text: string) {
-    const key = new NodeRSA(publicKey, 'public');
-    return key.encrypt(text, 'base64') as string;
+    const key = new NodeRSA(publicKey, "public");
+    return key.encrypt(text, "base64") as string;
   }
 
   private decryptWithPrivateKey(privateKey: string, encryptedText: string) {
-    const key = new NodeRSA(privateKey, 'private');
-    return key.decrypt(encryptedText, 'utf8') as string;
+    const key = new NodeRSA(privateKey, "private");
+    return key.decrypt(encryptedText, "utf8") as string;
   }
 }
